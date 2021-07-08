d2a193deeb67a35c90af78559650e8f8e5626e8d
a1564471347 <65760851+a1564471347@users.noreply.github.com>
2021-07-08 14:19:32 +0800

    Update and rename ddaydCOOKIE.js to dsjCOOKIE.js

---
+++ Task/dsjCOOKIE.js
@@ -0,0 +1,24 @@
+/* ziye
+独立COOKIE文件
+github地址 https://github.com/ziye888
+TG频道地址  https://t.me/ziyescript
+TG交流群   https://t.me/joinchat/AAAAAE7XHm-q1-7Np-tF3g
+boxjs链接  https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/ziye.boxjs.json
+圈X task订阅 https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/ziye-gallery.json
+
+转载请备注个名字，谢谢
+
+在boxjs里选择复制会话 粘贴至cookie处
+
+*/
+
+
+
+
+let cookie = {"id":"dsj","name":"电视家APP","keys":["dsjheader","dsjurl","dsjheader2","dsjurl2","dsjheader3","dsjurl3","dsjheader4","dsjurl4","dsjheader5","dsjurl5","dsjheader6","dsjurl6","dsjheader7","dsjurl7","dsjheader8","dsjurl8","dsjheader9","dsjurl9","dsjheader10","dsjurl10","dsjheader11","dsjurl11","dsjheader12","dsjurl12","dsjheader13","dsjurl13","dsjheader14","dsjurl14","dsjheader15","dsjurl15","dsjheader16","dsjurl16","dsjheader17","dsjurl17","dsjheader18","dsjurl18","dsjheader19","dsjurl19","dsjheader20","dsjurl20"],"author":"@ziye","settings":[{"id":"dsjSuffix","name":"当前账号","val":"1","type":"number","desc":"当前抓取ck记录的账号序号，如：1、2、3、"},{"id":"dsjCount","name":"账号个数","val":"3","type":"number","desc":"指定任务最多跑几个账号，根据抓取的账号数据个数来设值"},{"id":"dsjXH","name":"循环获取CK","val":"0","type":"number","desc":"0关闭 1开启,默认关闭"},{"id":"dsjXYZ","name":"执行概率","val":"100","type":"number","desc":"0不执行 可设置0-100,默认百分百"},{"id":"dsjnotifyttt","name":"推送控制","val":"1","type":"number","desc":"0关闭，1推送,默认12点以及23点推送"},{"id":"dsjnotifyInterval","name":"通知控制","val":"2","type":"number","desc":"0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知 "},{"id":"dsjMinutes","name":"推送-通知 分钟控制","val":"10","type":"number","desc":"推送以及通知控制在什么分钟段，可设置0-59,默认0到10"}],"repo":"https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/dsj.js","icons":["https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/dsj.png","https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/dsj.png"],"script":"https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/dsj.js","icon":"https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/dsj.png","favIcon":"mdi-star-outline","favIconColor":"grey","datas":[{"key":"dsjheader","val":"a31335b72f42d44f50a7148fe6f8e724&TnpSalpXVmlNRE5rTURNeFpEUTRNamxtWldJeFlXWXlZVEJsTkRFd05XRT18MTYyNDQyMjg1OTEzMjk0NDQ3NHxkNDkwZDNjYjY4N2ViODBlYzlhNzAxNzgxMTU3NTY4ZDQ3NWNlOTQ5"},{"key":"dsjurl","val":"a31335b72f42d44f50a7148fe6f8e724&xmCsEqtFPSqta6piQxg5gx1Im2Aea68addb61200f17bb0371f32ee4d7ab"},{"key":"dsjheader2","val":"07b307ece03c294c4aa2c323141c4bc6&T0dFMlpXWXpaVEJrTW1Vd1pqWmhObVl5TldFNU1HRmtPREV5WVRkaE1qTT18MTYyNDQyMzU1MzIwMjI3MTI1MHw2OWVhYzllZWEyYzM1NmZlZTljYmRhYjU3NjU0NjdkNDkzYTk0MTQ0"},{"key":"dsjurl2","val":"07b307ece03c294c4aa2c323141c4bc6&hjjLRZN2mrU1r6PPdeug37YO366u0k13e0ba224c28933391fb714e8e30ccc1"},{"key":"dsjheader3","val":"3e27bf437dec126bec00d4a43f9fff83&WVRrelpqUTRaR000Tm1VMlpXWTJPRGs0TldJd05XSmhNRGsyWVdJMU5XUT18MTYyNDQyMzcyMTUzMDczOTMxM3wzNzdlZTBmNWIyN2ExYmNjODlhYmIyYzk5YzU4YWEyMWM0NjVmMDk1"},{"key":"dsjurl3","val":"3e27bf437dec126bec00d4a43f9fff83&rR4Q7dWacd305b7e8a58b8a59d0827c26902434"},{"key":"dsjheader4","val":""},{"key":"dsjurl4","val":""},{"key":"dsjheader5","val":""},{"key":"dsjurl5","val":""},{"key":"dsjheader6","val":""},{"key":"dsjurl6","val":""},{"key":"dsjheader7","val":""},{"key":"dsjurl7","val":""},{"key":"dsjheader8","val":""},{"key":"dsjurl8","val":""},{"key":"dsjheader9","val":""},{"key":"dsjurl9","val":""},{"key":"dsjheader10","val":""},{"key":"dsjurl10","val":""},{"key":"dsjheader11","val":""},{"key":"dsjurl11","val":""},{"key":"dsjheader12","val":""},{"key":"dsjurl12","val":""},{"key":"dsjheader13","val":""},{"key":"dsjurl13","val":""},{"key":"dsjheader14","val":""},{"key":"dsjurl14","val":""},{"key":"dsjheader15","val":""},{"key":"dsjurl15","val":""},{"key":"dsjheader16","val":""},{"key":"dsjurl16","val":""},{"key":"dsjheader17","val":""},{"key":"dsjurl17","val":""},{"key":"dsjheader18","val":""},{"key":"dsjurl18","val":""},{"key":"dsjheader19","val":""},{"key":"dsjurl19","val":""},{"key":"dsjheader20","val":""},{"key":"dsjurl20","val":""}],"sessions":[],"isLoaded":true}
+
+
+
+
+
+module.exports = cookie
