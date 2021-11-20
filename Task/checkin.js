1f32711d27f432643ba19cc4ad5707536c438f3e
a1564471347 <65760851+a1564471347@users.noreply.github.com>
2021-11-20 11:35:49 +0800

    Update checkin.js

--- Task/checkin.js
+++ Task/checkin.js
@@ -21,7 +21,7 @@ const crypto = require('crypto');
 async function all() {
 
   $.log("【九章头条提现】");
-  let TokenArr = [''oHONvdSRTwAED_ZCGZoAcKewoESbFonM','6R4AfMADk2BUbIBU2wdghEFRedsIt7pv','8XQVGmOJr-kl45WsSzG8fEbc_C6oNY2l'];
+  let TokenArr = ['oHONvdSRTwAED_ZCGZoAcKewoESbFonM','6R4AfMADk2BUbIBU2wdghEFRedsIt7pv','8XQVGmOJr-kl45WsSzG8fEbc_C6oNY2l'];
 
   for (let i = 0; i < TokenArr.length; i++) {
     $.log(`开始执行第${i + 1}个账号`);
