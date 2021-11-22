4746dc6883eb6f46095ddfe9abe884b8fb442946
a1564471347 <65760851+a1564471347@users.noreply.github.com>
2021-11-23 01:53:28 +0800

    Update checkin.js

--- Task/checkin.js
+++ Task/checkin.js
@@ -104,7 +104,7 @@ function NineWithDraw() {
   return new Promise((resolve, reject) => {
     const url = "https://api.st615.com/v1/cash/withdraw-new";
     const headers = {};
-    const body = `token=${Token}&type=1&money=0.3`;
+    const body = `token=${Token}&type=1&money=30`;
     const request = {
       url: url,
       headers: headers,
